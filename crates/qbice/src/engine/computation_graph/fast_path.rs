0ead8c37aa5d0272a37428c2034b0d286b8ba04e
Simmypeet <simmypeet@outlook.com>
2025-12-22 19:30:33 +0700

    feat: early abort on scc

--- crates/qbice/src/engine/computation_graph/fast_path.rs
+++ crates/qbice/src/engine/computation_graph/fast_path.rs
@@ -1,9 +1,13 @@
 use std::sync::Arc;
 
 use crate::{
-    Engine, Query, config::Config,
-    engine::computation_graph::caller::CallerInformation,
-    executor::CyclicError, query::QueryID,
+    Engine, Query,
+    config::Config,
+    engine::computation_graph::{
+        caller::CallerInformation, computing_lock::Computing,
+    },
+    executor::CyclicError,
+    query::QueryID,
 };
 
 /// The result of attempting a fast-path query execution.
@@ -14,6 +18,60 @@ pub enum FastPathResult<V> {
 }
 
 impl<C: Config> Engine<C> {
+    /// Checks whether the stack of computing queries contains a cycle
+    fn check_cyclic(&self, computing: &Computing, target: QueryID) -> bool {
+        if computing.contains_query(&target) {
+            computing.mark_scc();
+
+            return true;
+        }
+
+        let mut found = false;
+
+        // OPTIMIZE: this can be parallelized
+        for dep in computing.registered_callees() {
+            let Some(state) =
+                self.computation_graph.computing_lock.try_get_lcok(dep)
+            else {
+                continue;
+            };
+
+            found |= self.check_cyclic(&state, target);
+        }
+
+        if found {
+            computing.mark_scc();
+        }
+
+        found
+    }
+
+    /// Exit early if a cyclic dependency is detected.
+    fn exit_scc(
+        &self,
+        called_from: Option<&QueryID>,
+        running_state: &Computing,
+    ) -> Result<(), CyclicError> {
+        // if there is no caller, we are at the root.
+        let Some(called_from) = called_from else {
+            return Ok(());
+        };
+
+        let is_in_scc = self.check_cyclic(running_state, *called_from);
+
+        // mark the caller as being in scc
+        if is_in_scc {
+            let computing =
+                self.computation_graph.computing_lock.get_lock(called_from);
+
+            computing.mark_scc();
+
+            return Err(CyclicError);
+        }
+
+        Ok(())
+    }
+
     pub(super) async fn fast_path<Q: Query>(
         self: &Arc<Self>,
         query_id: &QueryID,
@@ -22,6 +80,9 @@ impl<C: Config> Engine<C> {
         if let Some(computing) =
             self.computation_graph.computing_lock.try_get_lcok(query_id)
         {
+            // exit out of the scc query to avoid circular waits
+            self.exit_scc(caller.get_caller(), &computing)?;
+
             let notified_owned = computing.notified_owned();
             drop(computing);
 
