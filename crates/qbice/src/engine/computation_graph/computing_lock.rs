e9b6d1537f2a1f23de333895e9795ce43f596b48
Simmypeet <simmypeet@outlook.com>
2025-12-22 19:14:47 +0700

    feat: add register and abort callee

--- crates/qbice/src/engine/computation_graph/computing_lock.rs
+++ crates/qbice/src/engine/computation_graph/computing_lock.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::{HashMap, hash_map::Entry},
+    sync::Arc,
+};
 
-use dashmap::{DashMap, mapref::one::Ref};
+use dashmap::{
+    DashMap,
+    mapref::one::{Ref, RefMut},
+};
 use fxhash::FxHashSet;
 use qbice_serialize::{Decode, Encode};
 use qbice_stable_hash::Compact128;
@@ -8,7 +14,7 @@ use qbice_stable_type_id::Identifiable;
 use qbice_storage::intern::Interned;
 use tokio::sync::{Notify, futures::OwnedNotified};
 
-use crate::query::QueryID;
+use crate::{engine::computation_graph::QueryKind, query::QueryID};
 
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode, Identifiable)]
 pub struct TransitiveFirewallCallees(FxHashSet<QueryID>);
@@ -22,6 +28,36 @@ pub struct Observation {
 
 pub struct CalleeInfo {
     callee_queries: HashMap<QueryID, Option<Observation>>,
+    callee_order: Vec<QueryID>,
+    query_kind: QueryKind,
+}
+
+impl Computing {
+    pub fn register_calee(&mut self, callee: QueryID) {
+        match self.callee_info.callee_queries.entry(callee) {
+            Entry::Occupied(_) => {}
+
+            Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(None);
+
+                // if haven't inserted, add to dependency order
+                self.callee_info.callee_order.push(callee);
+            }
+        }
+    }
+
+    pub fn abort_callee(&mut self, callee: &QueryID) {
+        assert!(self.callee_info.callee_queries.remove(callee).is_some());
+        if let Some(pos) =
+            self.callee_info.callee_order.iter().position(|&id| id == *callee)
+        {
+            self.callee_info.callee_order.remove(pos);
+        }
+    }
+
+    pub fn query_kind(&self) -> QueryKind {
+        self.callee_info.query_kind.clone()
+    }
 }
 
 pub struct Computing {
